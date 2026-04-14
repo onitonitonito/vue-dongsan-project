584836f23eec405eaa7c75b8fc82b2fb7ec74333
�‍♂️suparX & spiegeL [Lenovo X1] <nitt0x0@gmail.com>
2026-04-15 00:12:07 +0900

    [2강] 반복문 {p, i} 쓰는법 -- 매물명, 가격 (데이터바인딩)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <HelloWorld msg="안녕하세요 Vue는 첨 입니다."></HelloWorld>
+  <!-- Navigation Menu -->
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+  </div>
+
+  <!-- Logo Section -->
+  <div class="logo-wrapper">
+    <img alt="Vue logo" src="./assets/logo-mario.png" class="logo">
+  </div>
+
+  <!-- Product List Section -->
+  <div class="content">
+    <div v-for="(p, i) in products" :key="i" class="product-item">
+      <h4>{{ i+1 }}. {{ p }}</h4>
+      <p>{{ prices[i] }}만원</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      메뉴들: ['Home', 'Shop', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      prices: [50, 60, 70],
+    }
   }
 }
 </script>
 
+<style>
+/* Component specific styles can go here, but I'll focus on main.css for general design */
+</style>
