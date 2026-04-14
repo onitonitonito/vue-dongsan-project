b5e49eb84613aecda8dfa7ba28a80355c3060392
�‍♂️suparX & spiegeL [Lenovo X1] <nitt0x0@gmail.com>
2026-04-14 23:45:42 +0900

    [잡일] 간단한 환경정화활동 및 배포

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="안녕하세요 Vue는 첨 입니다."></HelloWorld>
 </template>
 
 <script>
@@ -14,13 +13,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/vue-dongsan-project/',
+  plugins: [vue()],
+  server: {
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
